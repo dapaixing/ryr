17c2c642701a098db1209e7f3526944bb8b13b2b
dapaixing <53035807+dapaixing@users.noreply.github.com>
2020-02-26 21:05:53 +0800

    项目开始

--- test_webapp/src/main/java/dao/DBUtil.java
+++ test_webapp/src/main/java/dao/DBUtil.java
@@ -1,6 +1,12 @@
 package dao;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Arrays;
 
 public class DBUtil {
@@ -10,10 +16,41 @@ public class DBUtil {
 
     private static DataSource dataSource = null;
 
-    public static void main(String[] args) {
-        String s = null;
-        char[] chars = s.toCharArray();
-        Arrays.sort(chars);
-        String s1 = chars.toString();
+    public static DataSource getDataSource(){
+        if (dataSource == null){
+            synchronized (DBUtil.class) {
+                if (dataSource == null) {
+                    dataSource = new MysqlDataSource();
+                    MysqlDataSource tmpDataSource = (MysqlDataSource)dataSource;
+                    tmpDataSource.setURL(URL);
+                    tmpDataSource.setUser(USERNAME);
+                    tmpDataSource.setPassword(PASSWORD);
+                }
+            }
+        }
+        return dataSource;
+    }
+    public static java.sql.Connection getConnection()  {
+        try {
+            return getDataSource().getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static void close(Connection connection, PreparedStatement statement, ResultSet resultSet) {
+        try {
+            if (resultSet != null){
+                resultSet.close();
+            }
+            if (statement != null){
+                statement.close();
+            }
+            if (connection !=null){
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
