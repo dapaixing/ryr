a78efc795f8fec28836bc5e97590291cfca056eb
dapaixing <53035807+dapaixing@users.noreply.github.com>
2020-03-02 20:08:51 +0800

    后端构思

---
+++ test_webapp/src/main/java/api/ImageServlet.java
@@ -0,0 +1,16 @@
+package api;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ImageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(200);
+        //这个代码就是把hello这个字符串放到http相应的body中了
+        resp.getWriter().write("hello");
+    }
+}
