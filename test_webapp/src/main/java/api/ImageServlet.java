54060da432dbba0597e07a69c556163bee185f1d
dapaixing <53035807+dapaixing@users.noreply.github.com>
2020-03-05 21:30:44 +0800

    后端完善

--- test_webapp/src/main/java/api/ImageServlet.java
+++ test_webapp/src/main/java/api/ImageServlet.java
@@ -1,5 +1,7 @@
 package api;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import dao.Image;
 import dao.ImageDao;
 import org.apache.commons.fileupload.FileItem;
@@ -22,9 +24,45 @@ import java.util.List;
 public class ImageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setStatus(200);
-        //这个代码就是把hello这个字符串放到http相应的body中了
-        resp.getWriter().write("hello");
+        //考虑查看所有图片属性和查看指定图片属性
+        //通过URL中带有imageId参数来进行区分
+        //存在imageId查看指定图片属性，否则就查看所有图片属性
+        //例如：URL/image?imageId=100
+        //imageId的值就是"100"
+        //如果URL中不存在imageId那么返回null
+        String imageId = req.getParameter("imageId");
+        if (imageId == null || imageId.equals("")){
+            //查看所有图片属性
+            selectAll(req,resp);
+        }else {
+            //查看指定图片属性
+            selectOne(imageId,resp);
+        }
+    }
+
+    private void selectOne(String imageId, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json; charset=utf-8");
+        //1、创建一个ImageDao对象
+        ImageDao imageDao = new ImageDao();
+        Image image = imageDao.selectOne(Integer.parseInt(imageId));
+        //2、使用gson把查到的数据转换成json格式，并写回给响应对象
+        Gson gson = new GsonBuilder().create();
+        String jsonData = gson.toJson(image);
+        resp.getWriter().write(jsonData);
+    }
+
+    private void selectAll(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json; charset=utf-8");
+        //创建一个ImageDao对象，并查找数据库
+        ImageDao imageDao = new ImageDao();
+        List<Image> images = imageDao.selectAll();
+        //把查找到的结果转成JSON格式的字符串，并写回给resp对象
+        Gson gson = new GsonBuilder().create();
+        //  jsonData就是一个json格式的字符串了，就和之前约定的格式是一样的
+        //  gson帮助我们自动完成了大量的格式转换工作
+        //  只要把之前的相关字段都约定成统一命名，下面的操作就可以一步到位的完成整个转换
+        String jsonData = gson.toJson(images);
+        resp.getWriter().write(jsonData);
     }
 
     @Override
@@ -58,8 +96,8 @@ public class ImageServlet extends HttpServlet {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
         image.setUploadTime(simpleDateFormat.format(new Date()));
         image.setContentType(fileItem.getContentType());
-        //自己构造一个路径来保存
-        image.setPath("D:\\代码\\2020\\test_webapp\\src\\image\\"+image.getImageName());
+        //自己构造一个路径来保存，引入时间戳是为了让文件路径能够唯一
+        image.setPath("D:\\代码\\2020\\test_webapp\\src\\image\\"+System.currentTimeMillis()+""+image.getImageName());
         image.setMd5("11223344");
         //存入数据库
         ImageDao imageDao = new ImageDao();
@@ -81,6 +119,27 @@ public class ImageServlet extends HttpServlet {
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
+        //1、先获取到请求中的imageId
+        String imageId = req.getParameter("imageId");
+        if (imageId == null || imageId.equals("")){
+            resp.setStatus(200);
+            resp.getWriter().write("{\"ok\":false,\"reason\":\"解析请求失败\"}");
+            return;
+        }
+        //2、创建ImageDao对象，查看到该图片对象对应的相关属性（这是为了知道这个图片对应的文件路径）
+        ImageDao imageDao = new ImageDao();
+        Image image = imageDao.selectOne(Integer.parseInt(imageId));
+        if (image == null){
+            resp.setStatus(200);
+            resp.getWriter().write("{\"ok\":false,\"reason\":\"imageId在数据库中不存在\"}");
+            return;
+        }
+        //3、删除数据库中的记录
+        imageDao.delete(Integer.parseInt(imageId));
+        //4、删除本地磁盘文件
+        File file = new File(image.getPath());
+        file.delete();
+        resp.setStatus(200);
+        resp.getWriter().write("{\"ok\":true}");
     }
 }
